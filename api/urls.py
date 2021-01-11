a2535473803b7348aeb3a8e06683866a9768151e
Kegan Ronholt <klronholt@mail.fhsu.edu>
2021-01-11 13:22:36 -0600

    first push: backend RESTful api created

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+#from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.apiOverview, name="api-overview"),
+    path('task-list/', views.taskList, name="task-list"),
+    path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+    path('task-create/', views.createTask, name="task-create"),
+    path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
+    path('task-delete/<str:pk>/', views.taskDelete, name='task-delete'),
+   
+]
